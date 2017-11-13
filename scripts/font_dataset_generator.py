ab9a78861d397b9dde9228a9281e3cadb1614991
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2017-11-13 13:51:37 -0500

    Generate digit images

--- scripts/font_dataset_generator.py
+++ scripts/font_dataset_generator.py
@@ -1,74 +1,72 @@
+"""
+This script generates digit images from fonts
+All basic fonts are stored in folder "./fonts"
+Chosen fonts to export are in "fonts_list.txt"
+"""
+
 from PIL import Image, ImageDraw, ImageFont
-import ttfquery.findsystem
-import string
-import ntpath
-import numpy as np
 import os
 import glob
 
-fontSize = 20
-imgSize = (12, 20)
-position = (0, 0)
+# Font size
+fontSize = 90
 
-# All images will be stored in 'Synthetic_dataset' directory under current directory
-dataset_path = os.path.join(os.getcwd(), 'Synthetic_dataset')
+# Output image size
+imgSize = (130, 130)
+
+# Background color of the image
+background_color = (0, 0, 0)
+
+# Text color of the character
+char_color = (255, 255, 255)
+
+# Position of a char/digit on the image
+position = (15, 15)
+
+# All images will be stored in 'font_images' directory under current directory
+dataset_path = os.path.join(os.getcwd(), 'font_images')
 if not os.path.exists(dataset_path):
     os.makedirs(dataset_path)
 
-fhandle = open('Fonts_list.txt', 'r')
-lower_case_list = list(string.ascii_lowercase)
-upper_case_list = list(string.ascii_uppercase)
-digits = range(0, 10)
+# Store all chars/digits in an array
+chars_list = []
+for d in range(0, 10):
+    chars_list.append(str(d))
 
-digits_list = []
-for d in digits:
-    digits_list.append(str(d))
-
-all_char_list = lower_case_list + upper_case_list + digits_list
-
-fonts_list = []
+# Handle desired fonts
+fhandle = open('fonts_list.txt', 'r')
+desired_fonts = []
 for line in fhandle:
-    fonts_list.append(line.rstrip('\n'))
+    desired_fonts.append(line.rstrip('\n'))
 
-total_fonts = len(fonts_list)
-# paths = ttfquery.findsystem.findFonts()
-all_fonts = glob.glob0("~\\*.ttf")
-f_flag = np.zeros(total_fonts)
+# Handle all system fonts
+sys_fonts = glob.glob("./fonts/*.ttf")
 
-for sys_font in all_fonts:
-    # print "Checking "+p
-    font_file = ntpath.basename(sys_font)
-    font_file = font_file.rsplit('.')
-    font_file = font_file[0]
-    f_idx = 0
-    for font in fonts_list:
-        f_lower = font.lower()
+# Number of fonts
+font_count = 0
+
+for sys_font in sys_fonts:
+    # Only pick desired fonts
+    for desired_font in desired_fonts:
+        f_lower = desired_font.lower()
         s_lower = sys_font.lower()
-        # Check desired font
+
+        # Check if a desired font is in system fonts
         if f_lower in s_lower:
+            font_count += 1
             path = sys_font
-            font = ImageFont.truetype(path, fontSize)
-            f_flag[f_idx] = 1
-            for ch in all_char_list:
-                image = Image.new("RGB", imgSize, (255, 255, 255))
+            desired_font = ImageFont.truetype(path, fontSize)
+
+            for ch in chars_list:
+                # For each char, draw a blank image and put text on the image
+                image = Image.new("RGB", imgSize, background_color)
                 draw = ImageDraw.Draw(image)
-                pos_x = 0
-                pos_y = 0
-                pos_idx = 0
-                for y in [pos_y - 1, pos_y, pos_y + 1]:
-                    for x in [pos_x - 1, pos_x, pos_x + 1]:
-                        position = (x, y)
-                        draw.text(position, ch, (0, 0, 0), font=font)
-                        ##without this flag, it creates 'Calibri_a.jpg' even for 'Calibri_A.jpg'
-                        ##which overwrites lowercase images
-                        l_u_d_flag = "u"
-                        if ch.islower():
-                            l_u_d_flag = "l"
-                        elif ch.isdigit():
-                            l_u_d_flag = "d"
-                        file_name = font_file + '_' + l_u_d_flag + '_' + str(pos_idx) + '_' + ch + '.jpg'
-                        file_name = os.path.join(dataset_path, file_name)
-                        image.save(file_name)
-                        pos_idx = pos_idx + 1
-        f_idx = f_idx + 1
+                draw.text(position, ch, char_color, font=desired_font)
+
+                # Save the image
+                file_name = '%c_%i.jpg' % (ch, font_count)
+                file_name = os.path.join(dataset_path, file_name)
+                image.save(file_name)
+
+
 
