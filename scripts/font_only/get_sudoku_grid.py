5052828adc37a9eb0bce25af7246aeb70a5e6a76
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2017-11-21 01:00:29 -0500

    Recognize a sudoku grid

---
+++ scripts/font_only/get_sudoku_grid.py
@@ -0,0 +1,110 @@
+import predict_knn as knn
+import numpy as np
+import cv2
+import image_helper as imhelp
+
+
+def get_edges(cnt):
+    """
+    Helper func returns 4 edges from contours that mark the sudoku grid
+    :param cnt: contours
+    :return: an array of 4 points a, b, c, d
+    :rtp: [(int, int)]
+    """
+    a = [-1, -1]
+    b = [-1, -1]
+    c = [-1, -1]
+    d = [-1, -1]
+    for element in cnt:
+        point = element[0]
+        if a[0] == -1 or a[0] + a[1] > point[0] + point[1]:
+            a[0] = point[0]
+            a[1] = point[1]
+
+        if d[0] == -1 or d[0] + d[1] < point[0] + point[1]:
+            d[0] = point[0]
+            d[1] = point[1]
+
+        if b[0] == -1 or b[0] - b[1] < point[0] - point[1]:
+            b[0] = point[0]
+            b[1] = point[1]
+
+        if c[0] == -1 or c[1] - c[0] < point[1] - point[0]:
+            c[0] = point[0]
+            c[1] = point[1]
+
+    return a, b, c, d
+
+
+def get_grid(image_path):
+    """
+    Get a sudoku grid from an sudoku image
+    :param (String) image_path: path to sudoku image
+    :return: sudoku grid image after perspective transformation
+    :rtp: [[uint8]]
+    """
+    im = cv2.imread(image_path)
+
+    if im is None:
+        return
+
+    # If the image is too big, resize it
+    max_size = 800.0
+    if im.shape[0] > max_size or im.shape[1] > max_size:
+        if im.shape[0] > im.shape[1]:
+            ratio = max_size / im.shape[0]
+        else:
+            ratio = max_size / im.shape[1]
+        im = cv2.resize(im, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_AREA)
+
+    # Image process: blur, binary, threshold
+    blur = cv2.GaussianBlur(im, (11, 11), 0)
+    gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+    th = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+
+    # Find contours in the image
+    im2, contours, hierarchy = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    max_area = 0
+    max_cnt = None
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 50 and max_area < area:
+            max_area = area
+            max_cnt = cnt
+
+    a, b, c, d = get_edges(max_cnt)
+    size = int(np.sqrt(max_area))
+
+    pts1 = np.float32([a, b, c, d])
+    pts2 = np.float32([[0, 0], [size, 0], [0, size], [size, size]])
+
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+    sudoku_grid = cv2.warpPerspective(th, M, (size, size))
+
+    return sudoku_grid
+
+
+def parse_sudoku(grid, n=9):
+    box_len = len(grid) / n
+
+    for i in range(n):
+        for j in range(n):
+            # Get cell(i, j) from the grid
+            x = j * box_len
+            y = i * box_len
+            cell = grid[y: y + box_len, x: x + box_len]
+
+            # Remove the border by extracting only 80% of the image
+            x = int(box_len * 0.12)
+            y = int(box_len * 0.12)
+            w = h = int(box_len * 0.8)
+            cell = cell[y: y + h, x: x + w]
+
+            number = knn.recognize_one(im=cell, adapt_threshold=False)
+
+
+if __name__ == "__main__":
+    sudoku_grid = get_grid(image_path="test_imgs/sudoku_2.jpg")
+    imhelp.show_image(sudoku_grid)
+    parse_sudoku(grid=sudoku_grid, n=9)
